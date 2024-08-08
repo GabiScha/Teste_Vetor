9bc2fa2aa375d9c365f7ebd9962a45fd7e0cefd0
Gabriela Schaper <gabrielasch2409@gmail.com>
2024-08-08 17:00:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_1_pg72
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[5];
+
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine("Escreva o vetor correspondente a " + i);
+                vetor[i] = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine("O vetor " + i + " coresponde a " + vetor.GetValue(i));
+                 
+
+            }
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
